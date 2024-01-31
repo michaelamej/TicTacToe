c4603b75a0706f59bc97a1ad4c4bdb8ad3cdfef2
Michaela Presser <147770729+michaelamej@users.noreply.github.com>
2024-01-30 20:37:41 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import TicTacToe from './components/tttoe'
+
+function App() {
+  return <TicTacToe/>;  
+}
+
+export default App
---
+++ src/components/Reset.jsx
@@ -0,0 +1,15 @@
+import GameState from "./GameState";
+
+function Reset({ gameState, onReset }) {
+    if(gameState === GameState.inProgress){
+        return;
+    }
+    return ( 
+    <button onClick={onReset} className="reset-button">
+        Reset
+        </button> 
+        
+    );
+}
+
+export default Reset;
